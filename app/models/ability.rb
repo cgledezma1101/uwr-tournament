847ffed22fe7c45e9d9fc6258359e15d15d1c2d5
Carlos Ledezma <cgledezma1101@gmail.com>
2015-05-16 16:02:29 +1000

    Added administration navbar for clubs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,10 @@ class Ability
       user.clubs.where{ id == my{club.id} }.any? || user.administrated_clubs.where{ id == my{club.id} }.any?
     end
 
+    can :edit, Club do |club|
+      user.administrated_clubs.where{ id == my{club.id} }.any?
+    end
+
     can :new, Club
     can :create, Club
   end
