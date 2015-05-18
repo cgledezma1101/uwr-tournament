ff270040b2076218dc757310b12b2b2cc0d81ec9
Carlos Ledezma <cgledezma1101@gmail.com>
2015-05-18 21:59:16 +1000

    Added capability to invite users to join your club

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,14 @@
 class InvitationsController < ApplicationController
-  # GET /invitations/new
+  # GET /invitations/new?club_id=:club_id&is_admin=:is_admin
+  #
+  # @param [Integer] club_id Identifier of the club the invitation is being created for
+  # @param [Boolean] is_admin Boolean indicating whether this is an invitation to be administrator
   def new
+    club = Club.find(params[:club_id])
+    authorize! :edit, club
+
+    @invitation = Invitation.new(club: club, is_admin: params[:is_admin])
+
     respond_to do |format|
       format.html{ render 'invitations/_new', layout: false }
     end
@@ -8,6 +16,22 @@ class InvitationsController < ApplicationController
 
   # POST /invitations
   def create
+    club = Club.find(params[:invitation][:club_id])
+    authorize! :edit, club
+
+    user = User.find_by(email: params[:user][:email])
+
+    if(user.nil?)
+      invitation = Invitation.create(club: club, is_admin: params[:invitation][:is_admin])
+    else
+      invitation = Invitation.create(club: club, user: user, is_admin: params[:invitation][:is_admin])
+    end
+
+    if(invitation.valid?)
+      redirect_to club_path(club), notice: t('invitations.create_success')
+    else
+      redirect_to club_path(club), alert: t('invitations.create_failure')
+    end
   end
 
   # GET /invitations/:id/accept
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,4 +4,19 @@ class Invitation < ActiveRecord::Base
 
   validates :club, presence: true
   validates :user, presence: true
+
+  validate :one_invitation_per_club
+
+  private
+
+  # Validates the uniqueness of the user in the scope of the club
+  def one_invitation_per_club
+    if !user.nil?
+      duplicate = Invitation.find_by(club: self.club, user: self.user)
+      
+      if !duplicate.nil? && duplicate.id != self.id
+        self.errors.add(:user, I18n.t('invitations.only_one_per_club'))
+      end
+    end
+  end
 end
