015d61551dbf7fd519d77f3f9e05104bd9e1229d
Carlos Ledezma <cgledezma1101@gmail.com>
2015-05-18 22:09:52 +1000

    Added further validations to invitation creation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,16 +6,28 @@ class Invitation < ActiveRecord::Base
   validates :user, presence: true
 
   validate :one_invitation_per_club
+  validate :cant_invite_members
 
   private
 
   # Validates the uniqueness of the user in the scope of the club
   def one_invitation_per_club
-    if !user.nil?
+    if !self.user.nil? && !self.club.nil?
       duplicate = Invitation.find_by(club: self.club, user: self.user)
-      
+
       if !duplicate.nil? && duplicate.id != self.id
         self.errors.add(:user, I18n.t('invitations.only_one_per_club'))
+      end
+    end
+  end
+
+  # Validates that an invitation is not sent to a person that's already part of the club
+  def cant_invite_members
+    if !self.user.nil? && !self.club.nil?
+      is_member = self.club.members.any?{ |member| member.id == self.user.id }
+
+      if is_member
+        self.errors.add(:user, I18n.t('invitations.cant_invite_members'))
       end
     end
   end
