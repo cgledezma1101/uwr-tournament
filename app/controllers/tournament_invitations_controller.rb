540e437e66fb4313b281747ad28caa3684b5d25f
Carlos Ledezma <cgledezma1101@gmail.com>
2015-09-05 20:08:31 +1000

    Added ability to invite a club to a tournament

--- app/controllers/tournament_invitations_controller.rb
+++ app/controllers/tournament_invitations_controller.rb
@@ -1,4 +1,23 @@
 class TournamentInvitationsController < ApplicationController
+  # POST /tournament_invitations
+  #
+  # Allows the creation of an invitation of a club to a tournament
+  #
+  # @param [Integer] club_id Identifier of the club to be invited
+  # @param [Integer] tournament_id Identifier of the tournament the club is being invited to
+  def create
+    invitation = TournamentInvitation.new(create_params)
+    authorize! :update, invitation.tournament
+
+    if invitation.save
+      redirect_params = { notice: t('tournament_invitation.create_success', club_name: invitation.club.name, tournament_name: invitation.tournament.name) }
+    else
+      redirect_params = { alert: t('tournament_invitation.create_failure') }
+    end
+
+    redirect_to tournament_path(invitation.tournament), redirect_params
+  end
+
   # GET /tournament_invitations?tournament_id=:tournament_id
   #
   # Renders a form that allows an administrator to invite a club to a tournament
@@ -12,7 +31,14 @@ class TournamentInvitationsController < ApplicationController
 
     tournament_clubs_ids = tournament.clubs.select{ |club| club.id }
     @eligible_clubs = Club.where{ id << my{tournament_clubs_ids} }
-    
+
     render 'tournament_invitations/_new', layout: false
+  end
+
+  private
+
+  # Sanitized parameters to create a new tournament invitation
+  def create_params
+    params.require(:tournament_invitation).permit(:club_id, :tournament_id)
   end
 end
