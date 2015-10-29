3074d6ee6f4c69591e031d4db2f7825cf4dd0d10
Carlos Ledezma <cgledezma1101@gmail.com>
2015-10-29 20:38:53 +1100

    Reverted tabs to spaces

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -4,155 +4,155 @@ var activeChronometers = 0;
 
 var updateChronometers = function()
 {
-	$.each(registeredChronometers, function(chronometerId, chronometer)
-	{
-		if (!chronometer)
-		{
-			return true;
-		}
+  $.each(registeredChronometers, function(chronometerId, chronometer)
+  {
+    if (!chronometer)
+    {
+      return true;
+    }
 
-		var minutesInput = chronometer.find('.js-chronometer-minutes');
-		var secondsInput = chronometer.find('.js-chronometer-seconds');
+    var minutesInput = chronometer.find('.js-chronometer-minutes');
+    var secondsInput = chronometer.find('.js-chronometer-seconds');
 
-		var currentMinutes = +(minutesInput.val());
-		var currentSeconds = +(secondsInput.val());
+    var currentMinutes = +(minutesInput.val());
+    var currentSeconds = +(secondsInput.val());
 
-		--currentSeconds;
+    --currentSeconds;
 
-		if (currentMinutes === 0 && currentSeconds <= 15)
-		{
-			minutesInput.addClass('timer-low');
-			secondsInput.addClass('timer-low');
-		}
+    if (currentMinutes === 0 && currentSeconds <= 15)
+    {
+      minutesInput.addClass('timer-low');
+      secondsInput.addClass('timer-low');
+    }
 
-		if (currentSeconds === -1)
-		{
-			if (currentMinutes === 0)
-			{
-				chronometer.find('.js-chronometer-stop').click();
-			}
-			else
-			{
-					minutesInput.val(--currentMinutes);
-					secondsInput.val(59);
-			}
-		}
-		else
-		{
-			secondsInput.val(currentSeconds);
-		}
+    if (currentSeconds === -1)
+    {
+      if (currentMinutes === 0)
+      {
+        chronometer.find('.js-chronometer-stop').click();
+      }
+      else
+      {
+          minutesInput.val(--currentMinutes);
+          secondsInput.val(59);
+      }
+    }
+    else
+    {
+      secondsInput.val(currentSeconds);
+    }
 
-		return true;
-	});
+    return true;
+  });
 }
 
 var chronometerStart = function()
 {
-	var chronometer = $(this).parents('.js-chronometer');
-	var name = chronometer.find('.js-chronometer-name');
-	var minutes = chronometer.find('.js-chronometer-minutes');
-	var seconds = chronometer.find('.js-chronometer-seconds');
+  var chronometer = $(this).parents('.js-chronometer');
+  var name = chronometer.find('.js-chronometer-name');
+  var minutes = chronometer.find('.js-chronometer-minutes');
+  var seconds = chronometer.find('.js-chronometer-seconds');
 
-	chronometerId = name.val();
+  chronometerId = name.val();
 
-	if (!chronometerId || (registeredChronometers[chronometerId] && !chronometer.hasClass('js-chronometer-playing')))
-	{
-		name.addClass('chronometer-error');
-		return false;
-	}
+  if (!chronometerId || (registeredChronometers[chronometerId] && !chronometer.hasClass('js-chronometer-playing')))
+  {
+    name.addClass('chronometer-error');
+    return false;
+  }
 
-	name.removeClass('chronometer-error');
+  name.removeClass('chronometer-error');
 
-	minutes.prop("readonly", "readonly");
-	seconds.prop("readonly", "readonly");
-	name.prop("readonly", "readonly");
+  minutes.prop("readonly", "readonly");
+  seconds.prop("readonly", "readonly");
+  name.prop("readonly", "readonly");
 
-	if (activeChronometers === 0)
-	{
-		chronometerInterval = window.setInterval(updateChronometers, 1000);
-	}
+  if (activeChronometers === 0)
+  {
+    chronometerInterval = window.setInterval(updateChronometers, 1000);
+  }
 
-	if (!registeredChronometers[chronometerId])
-	{
-		chronometer.addClass('js-chronometer-playing');
-		registeredChronometers[chronometerId] = chronometer;
-		++activeChronometers;
-	}
+  if (!registeredChronometers[chronometerId])
+  {
+    chronometer.addClass('js-chronometer-playing');
+    registeredChronometers[chronometerId] = chronometer;
+    ++activeChronometers;
+  }
 
-	// Prevent default behaviour and stop event propagation
-	return false;
+  // Prevent default behaviour and stop event propagation
+  return false;
 };
 
 var chronometerStop = function()
 {
-	var chronometer = $(this).parents('.js-chronometer');
-	var minutes = chronometer.find('.js-chronometer-minutes');
-	var seconds = chronometer.find('.js-chronometer-seconds');
-	var name = chronometer.find('.js-chronometer-name');
+  var chronometer = $(this).parents('.js-chronometer');
+  var minutes = chronometer.find('.js-chronometer-minutes');
+  var seconds = chronometer.find('.js-chronometer-seconds');
+  var name = chronometer.find('.js-chronometer-name');
 
-	chronometer.removeClass('js-chronometer-playing');
+  chronometer.removeClass('js-chronometer-playing');
 
-	minutes.removeClass('timer-low');
-	minutes.prop("readonly", "");
+  minutes.removeClass('timer-low');
+  minutes.prop("readonly", "");
 
-	seconds.removeClass('timer-low');
-	seconds.prop("readonly", "");
+  seconds.removeClass('timer-low');
+  seconds.prop("readonly", "");
 
-	name.prop("readonly", "");
+  name.prop("readonly", "");
 
-	var chronometerId = name.val();
-	if (registeredChronometers[chronometerId])
-	{
-		registeredChronometers[chronometerId] = undefined
-		--activeChronometers;
+  var chronometerId = name.val();
+  if (registeredChronometers[chronometerId])
+  {
+    registeredChronometers[chronometerId] = undefined
+    --activeChronometers;
 
-		if (activeChronometers === 0)
-		{
-			window.clearInterval(chronometerInterval);
-		}
-	}
+    if (activeChronometers === 0)
+    {
+      window.clearInterval(chronometerInterval);
+    }
+  }
 
-	// Prevent default behaviour and stop event propagation
-	return false;
+  // Prevent default behaviour and stop event propagation
+  return false;
 };
 
 var chronometerRemove = function()
 {
-	var chronometer = $(this).parents('.js-chronometer');
-	chronometer.find('.js-chronometer-stop').click();
-	chronometer.remove();
+  var chronometer = $(this).parents('.js-chronometer');
+  chronometer.find('.js-chronometer-stop').click();
+  chronometer.remove();
 
-	// Prevent default behaviour and stop event propagation
-	return false;
+  // Prevent default behaviour and stop event propagation
+  return false;
 }
 
 var addToFeeds = function(text)
 {
-	var event_form = $('#new_game_event').first();
-	var input_field = event_form.find('.js-event-text');
-	var old_text = input_field.val();
-	input_field.val(text);
-	event_form.submit();
-	input_field.val(old_text);
+  var event_form = $('#new_game_event').first();
+  var input_field = event_form.find('.js-event-text');
+  var old_text = input_field.val();
+  input_field.val(text);
+  event_form.submit();
+  input_field.val(old_text);
 }
 
 $(document).ready(function()
 {
-	$('.js-chronometer-stopall').on('click', function()
-	{
-		var panel = $(this).parents('.js-chronometers-panel');
-		panel.find('.js-chronometer-stop').click();
+  $('.js-chronometer-stopall').on('click', function()
+  {
+    var panel = $(this).parents('.js-chronometers-panel');
+    panel.find('.js-chronometer-stop').click();
 
-		// Prevent default behaviour and stop event propagation
-		return false;
-	});
+    // Prevent default behaviour and stop event propagation
+    return false;
+  });
 
-	$('.js-chronometer-resumeall').on('click', function()
-	{
-		var panel = $(this).parents('.js-chronometers-panel');
-		panel.find('.js-chronometer-start').click();
+  $('.js-chronometer-resumeall').on('click', function()
+  {
+    var panel = $(this).parents('.js-chronometers-panel');
+    panel.find('.js-chronometer-start').click();
 
-		// Prevent default behaviour and stop event propagation
-		return false;
-	});
+    // Prevent default behaviour and stop event propagation
+    return false;
+  });
 });
