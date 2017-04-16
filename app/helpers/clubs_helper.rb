c1241ef82af1273658c16de9ab9bd371d28151c0
Carlos Ledezma <cgledezma1101@gmail.com>
2017-04-16 17:36:04 +1000

    Fixed redundant helper name

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -4,7 +4,7 @@ module ClubsHelper
 	# @param [Club] club The club for which the actions will be performed
 	#
 	# @return [Proc] A method that can be called with a particular administrator and that will render the actions that can be performed on that administrator for the mentioned club
-	def create_admin_actions_proc(club)
+	def create_club_admin_actions_proc(club)
 		Proc.new do |user|
 			club_admin = ClubAdmin.find_by(club: club, user: user)
 			render 'clubs/admin_actions', club_admin: club_admin
