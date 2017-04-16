fcf0bc232fe5be0493c7f774fce65e6b999dc503
Carlos Ledezma <cgledezma1101@gmail.com>
2017-04-16 17:24:38 +1000

    Upgraded to 4.0.13 to deal with date_select problem

--- db/schema.rb
+++ db/schema.rb
@@ -13,142 +13,142 @@
 
 ActiveRecord::Schema.define(version: 20151029080318) do
 
-	create_table "club_admins", force: true do |t|
-		t.integer  "user_id"
-		t.integer  "club_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "club_admins", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "club_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "club_join_requests", force: true do |t|
-		t.integer  "user_id"
-		t.integer  "club_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "club_join_requests", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "club_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "clubs", force: true do |t|
-		t.string   "name"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "clubs", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "game_events", force: true do |t|
-		t.string   "text"
-		t.integer  "game_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "game_events", force: true do |t|
+    t.string   "text"
+    t.integer  "game_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "games", force: true do |t|
-		t.integer  "blue_team_id"
-		t.integer  "white_team_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-		t.string   "winning_color"
-		t.integer  "stage_id"
-		t.string   "status"
-	end
+  create_table "games", force: true do |t|
+    t.integer  "blue_team_id"
+    t.integer  "white_team_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "winning_color"
+    t.integer  "stage_id"
+    t.string   "status"
+  end
 
-	create_table "invitations", force: true do |t|
-		t.integer  "club_id"
-		t.integer  "user_id"
-		t.boolean  "is_admin"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "invitations", force: true do |t|
+    t.integer  "club_id"
+    t.integer  "user_id"
+    t.boolean  "is_admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "player_games", force: true do |t|
-		t.integer "player_id"
-		t.integer "game_id"
-		t.string  "team_color"
-	end
+  create_table "player_games", force: true do |t|
+    t.integer "player_id"
+    t.integer "game_id"
+    t.string  "team_color"
+  end
 
-	create_table "players", force: true do |t|
-		t.integer "number"
-		t.integer "team_id"
-		t.integer "user_id"
-		t.boolean "is_active"
-	end
+  create_table "players", force: true do |t|
+    t.integer "number"
+    t.integer "team_id"
+    t.integer "user_id"
+    t.boolean "is_active"
+  end
 
-	add_index "players", ["user_id"], name: "index_players_on_user_id"
+  add_index "players", ["user_id"], name: "index_players_on_user_id"
 
-	create_table "scores", force: true do |t|
-		t.integer  "player_id"
-		t.integer  "game_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "scores", force: true do |t|
+    t.integer  "player_id"
+    t.integer  "game_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "stages", force: true do |t|
-		t.integer  "tournament_id"
-		t.string   "name"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "stages", force: true do |t|
+    t.integer  "tournament_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "teams", force: true do |t|
-		t.string  "name"
-		t.integer "club_id"
-	end
+  create_table "teams", force: true do |t|
+    t.string  "name"
+    t.integer "club_id"
+  end
 
-	add_index "teams", ["club_id"], name: "index_teams_on_club_id"
+  add_index "teams", ["club_id"], name: "index_teams_on_club_id"
 
-	create_table "tournament_admins", force: true do |t|
-		t.integer  "user_id"
-		t.integer  "tournament_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "tournament_admins", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tournament_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "tournament_invitations", force: true do |t|
-		t.integer  "club_id"
-		t.integer  "tournament_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "tournament_invitations", force: true do |t|
+    t.integer  "club_id"
+    t.integer  "tournament_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "tournament_teams", force: true do |t|
-		t.integer  "tournament_id"
-		t.integer  "team_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "tournament_teams", force: true do |t|
+    t.integer  "tournament_id"
+    t.integer  "team_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "tournaments", force: true do |t|
-		t.string   "name"
-		t.date     "start_date"
-		t.date     "end_date"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "tournaments", force: true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "user_clubs", force: true do |t|
-		t.integer  "user_id"
-		t.integer  "club_id"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-	end
+  create_table "user_clubs", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "club_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-	create_table "users", force: true do |t|
-		t.string   "email",                  default: "", null: false
-		t.string   "encrypted_password",     default: "", null: false
-		t.string   "reset_password_token"
-		t.datetime "reset_password_sent_at"
-		t.datetime "remember_created_at"
-		t.integer  "sign_in_count",          default: 0,  null: false
-		t.datetime "current_sign_in_at"
-		t.datetime "last_sign_in_at"
-		t.string   "current_sign_in_ip"
-		t.string   "last_sign_in_ip"
-		t.datetime "created_at"
-		t.datetime "updated_at"
-		t.string   "name"
-		t.string   "provider"
-		t.string   "uid"
-	end
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+  end
 
-	add_index "users", ["email"], name: "index_users_on_email", unique: true
-	add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
