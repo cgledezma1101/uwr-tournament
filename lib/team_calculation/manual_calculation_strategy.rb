d4a9226aead984eade3e5b5b20aeb584981f3ea5
Carlos Ledezma <cgledezma1101@gmail.com>
2018-05-20 17:37:28 +1000

    Added nil handling in ManualCalculationStrategy

--- lib/team_calculation/manual_calculation_strategy.rb
+++ lib/team_calculation/manual_calculation_strategy.rb
@@ -1,9 +1,9 @@
 class TeamCalculation::ManualCalculationStrategy < TeamCalculation::CalculationStrategy
 	def calculate_team(game, team_color)
 		if (team_color == PlayerGame::WHITE_TEAM)
-			return Team.find(game.white_team_id)
+			return game.white_team_id.nil? ? nil : Team.find(game.white_team_id)
 		elsif (team_color == PlayerGame::BLUE_TEAM)
-			return Team.find(game.blue_team_id)
+			return game.blue_team_id.nil? ? nil : Team.find(game.blue_team_id)
 		end
 
 		raise "Invalid team color provided to calculation strategy: #{team_color}"
