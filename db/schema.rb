76d091263092c77e4eef4b884786818d22b623d3
Carlos Ledezma <cgledezma1101@gmail.com>
2018-05-20 14:49:41 +1000

    Added game_type column to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919074927) do
+ActiveRecord::Schema.define(version: 20180520044822) do
 
   create_table "club_admins", force: true do |t|
     t.integer  "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170919074927) do
     t.integer  "stage_id"
     t.string   "status"
     t.datetime "starts_at"
+    t.integer  "game_type"
   end
 
   create_table "invitations", force: true do |t|
