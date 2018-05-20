6d8357f8e28acc7eae5e6343d9947f651236bcc0
Carlos Ledezma <cgledezma1101@gmail.com>
2018-05-20 15:59:48 +1000

    Fixed typo in definition of CalculationStrategy

--- lib/team_calculation/calculation_strategy.rb
+++ lib/team_calculation/calculation_strategy.rb
@@ -4,7 +4,7 @@ class TeamCalculation::CalculationStrategy
 	# @param [Game] game The game for which the  team should be calculated
 	# @param [String] team_color PlayerGame::BLUE_TEAM or PlayerGame::WHITE_TEAM, depending on which team wants to be determined
 	# @return Team The calculated team
-	def calculate_team(:game, :team_color)
+	def calculate_team(game, team_color)
 		raise "calculate_team not implemented"
 	end
 end
