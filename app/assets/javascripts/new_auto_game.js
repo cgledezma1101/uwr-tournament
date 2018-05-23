13525298e9d87c7cdb38d76a0b81a24c935aa576
Carlos Ledezma <cgledezma1101@gmail.com>
2018-05-23 21:07:29 +1000

    Implemented JS to update fields required to properly create a calculated game based off game results

--- app/assets/javascripts/new_auto_game.js
+++ app/assets/javascripts/new_auto_game.js
@@ -8,4 +8,16 @@ $(document).on('change', '#newAutoGame .js-team-calculation-param', function(eve
 
 	var strategyString = "l|" + positionParam + "|" + stageParam;
 	$($stageContainer.data('target-calculation')).attr('value', strategyString);
-})
+});
+
+$(document).on('change', '#newAutoGameResult .js-team-calculation-param', function(event) {
+	event.preventDefault();
+	event.stopPropagation();
+
+	var $stageContainer = $(event.currentTarget).parents('.stage-container');
+	var gameParam = $stageContainer.find('.js-game-param').val();
+	var outcomeParam = $stageContainer.find('.js-outcome-param').val();
+
+	var strategyString = "g|" + gameParam + "|" + outcomeParam;
+	$($stageContainer.data('target-calculation')).attr('value', strategyString);
+});
