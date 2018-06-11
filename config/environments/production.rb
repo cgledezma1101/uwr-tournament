910f23554627aae68fc959c546fd3396b08089f0
Carlos Ledezma <cgledezma1101@gmail.com>
2018-06-11 13:16:13 +1000

    Sendgrid options for password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,12 @@ UwrTournament::Application.configure do
 	# Use default logging formatter so that PID and timestamp are not suppressed.
 	config.log_formatter = ::Logger::Formatter.new
 
-	config.action_mailer.default_url_options = { host: 'uwr.herokuapp.com' }	
+	config.action_mailer.default_url_options = { host: 'uwr-testing.herokuapp.com' }
+	ActionMailer::Base.smtp_settings = {
+		:address => "smtp.sendgrid.net",
+		:port => "25",
+		:authentication => :plain,
+		:user_name => ENV['SENDGRID_USERNAME'],
+		:password => ENV['SENDGRID_PASSWORD']
+	  }
 end
