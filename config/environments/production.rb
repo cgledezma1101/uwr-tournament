bc4e5452493769bd03c632920904ad16c851020b
Carlos Ledezma <cgledezma1101@gmail.com>
2018-06-11 13:24:50 +1000

    Devise configurations for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ UwrTournament::Application.configure do
 	# Use default logging formatter so that PID and timestamp are not suppressed.
 	config.log_formatter = ::Logger::Formatter.new
 
-	config.action_mailer.default_url_options = { host: 'uwr-testing.herokuapp.com' }
+	config.action_mailer.default_url_options = { host: 'uwr.herokuapp.com' }
 	ActionMailer::Base.smtp_settings = {
 		:address => "smtp.sendgrid.net",
 		:port => "25",
