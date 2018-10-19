1850cd02e65ec4ea528024fe99ae61412fa70968
Carlos Ledezma <cgledezma1101@gmail.com>
2018-10-19 12:23:06 +1100

    Event feed now clears the text of the event once the value is saved

--- app/views/game_events/create.js
+++ app/views/game_events/create.js
@@ -7,8 +7,9 @@
 */
 <% if @game_event.persisted? %>
 	var event_feeds = $('.event-feed');
-	var newEvent = $("<%= j render 'game_events/feed_item', game_event: @game_event %>")
+	var newEvent = $("<%= j render 'game_events/feed_item', game_event: @game_event %>");
 	newEvent.hide().appendTo(event_feeds).fadeIn();
+	event_feeds.parent().find('.js-event-text').val("");
 <% else %>
 	event_feeds.parent().find('.js-event-text').addClass('chronometer-error');
 <% end %>
